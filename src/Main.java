93258d169a338af4ad2686a07ea40c7a95de9761
Gleb <Sephirothash@yandex.ru>
2023-07-31 23:21:43 +0300

    bonusmileservice

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,9 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-        int check = 100;
-        int replenishment = 1400;
-
-        int bonus = replenishment / 100;
-
-        if (replenishment < 1000) {
-            bonus = 0;
-        }
-
-
-        System.out.println(check + replenishment);
-        System.out.println(bonus);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
-
 }
